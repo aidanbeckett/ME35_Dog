c88073fe70946f320283c79969031074e7c3ad42
nullThink <legoking43@gmail.com>
2023-04-02 17:09:04 -0400

    general reading "serial" structure set up. in need of testing

--- readFromCamera.py
+++ readFromCamera.py
@@ -1,18 +1,40 @@
 import asyncio
+import machine
+
+# Send out to External RP2040
+analogOutput = machine.Pin(26, machine.Pin.OUT)
+
+# Read from Camera
+analogInput = machine.Pin(27, machine.Pin.IN)
+
+reading = 0
+
+WALKING = 0
+SIT = 0
 
 async def read_camera():
+    global reading
+
     while True:
         #read camera
+        reading = analogInput.read()
         await asyncio.sleep(0.1)
         
 async def walking():
+    global reading
+
     while True:
-        #walk (analog write)
+        if(reading == WALKING):
+            analogOutput.write(50)
         await asyncio.sleep(0.1)
 
 async def sit():
+    global reading
+    
     while True:
-        # sit (analow write)
+        # sit (analog write)
+        if(reading == SIT):
+            analogOutput.write(100)
         await asyncio.sleep(0.1)
 
 async def main(duration):
--- readFromRP2040.py
+++ readFromRP2040.py
@@ -1,18 +1,41 @@
 import asyncio
+import machine
+
+#import walk function from external file on board. same for sit
+
+# Read from RP2040
+analogInput = machine.Pin(27, machine.Pin.IN)
+
+reading = 0
+
+WALKING = 0
+SIT = 0
 
 async def read_2040():
+    global reading
+
     while True:
-        #read 2040
+        #read camera
+        reading = analogInput.read()
         await asyncio.sleep(0.1)
         
 async def walking():
+    global reading
+
     while True:
-        #walk (analog write)
+        if(reading == WALKING):
+            #execute walk function
+            walk()
         await asyncio.sleep(0.1)
 
 async def sit():
+    global reading
+    
     while True:
-        # sit (analow write)
+        # sit (analog write)
+        if(reading == SIT):
+            #execute sit function
+            sittingFunction()
         await asyncio.sleep(0.1)
 
 async def main(duration):
